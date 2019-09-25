526326b36a2bcaf860435eaa2cf05208a2b03fc3
ArjenAch <arjenachterveld@hotmail.com>
2019-09-25 10:47:30 +0200

    Add project files.

---
+++ Life.Data/LifeDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Life.Data
+{
+    public class LifeDbContext : DbContext
+    {
+        public LifeDbContext(DbContextOptions<LifeDbContext> options) : base(options)
+        {
+        }
+    }
+}
+
---
+++ Life/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Life.Data;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Life
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = CreateWebHostBuilder(args).Build();
+
+            MigrateDatabase(host);
+
+            host.Run();
+
+        }
+
+        /// <summary>
+        /// Todo: ensure a backup is made before migrating
+        /// </summary>
+        /// <param name="host"></param>
+        private static void MigrateDatabase(IWebHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var db = scope.ServiceProvider.GetRequiredService<LifeDbContext>();
+                db.Database.Migrate();
+            }
+        }
+
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
