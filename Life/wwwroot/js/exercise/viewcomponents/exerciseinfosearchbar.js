a5f8a00e01bb188355ca548d3e0e4a88818867e0
ArjenAch <arjenachterveld@hotmail.com>
2019-10-16 15:02:01 +0200

    adds select functionality to the autocomplete search bar & sets values

--- Life/wwwroot/js/exercise/viewcomponents/exerciseinfosearchbar.js
+++ Life/wwwroot/js/exercise/viewcomponents/exerciseinfosearchbar.js
@@ -7,12 +7,25 @@
                 data = ($.map(data, function (obj, key) {
                     return {
                         label: obj.title,
-                        value: obj.title
+                        value: obj.title,
+                        id: obj.id,
+                        type: obj.exerciseType,
+                        description: obj.description
                     };
                 }));
 
                 $('#info-title').autocomplete({
-                    source: data
+                    source: data,
+                    select: function (event, ui) {
+                        //set values
+                        $('#exercise-type').val(ui.item.type);
+                        $('#description').val(ui.item.description);
+
+                        $(this).parent()
+                            .next()
+                            .find('input, textarea, select')
+                            .attr('disabled', true);
+                    }
                 });
             },
             error: function (error) {
