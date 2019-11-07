1cb58de18855474ba74f165fa926a79646f7b745
ArjenAch <arjenachterveld@hotmail.com>
2019-11-07 15:41:30 +0100

    adds concept overlay bar with drag and drop items

--- Life/wwwroot/js/exercise/workoutschedule.js
+++ Life/wwwroot/js/exercise/workoutschedule.js
@@ -1,1 +1,14 @@
-﻿
+﻿function drop(ev) {
+    ev.preventDefault();
+    var itemId = ev.dataTransfer.getData("itemId");
+    var list = ev.target.getElementsByTagName("ul")[0];
+    list.appendChild(document.getElementById(itemId).cloneNode(true));
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("itemId", ev.target.id);
+}
+
+function allowDrop(ev) {
+    ev.preventDefault();
+}
