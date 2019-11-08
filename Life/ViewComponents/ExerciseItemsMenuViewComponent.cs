5b2161ee47660bcf0d20cf14c1c4554be8f0c86f
ArjenAch <arjenachterveld@hotmail.com>
2019-11-08 11:41:03 +0100

    replaces hardcoded items in schedule view with a viewcomponent with dynamic data

---
+++ Life/ViewComponents/ExerciseItemsMenuViewComponent.cs
@@ -0,0 +1,28 @@
+﻿using Life.Application.Services.Exercises.DTO;
+using Life.Application.Services.Interfaces.Exercises;
+using Life.Core.Domain.Exercises;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Life.ViewComponents
+{
+    public class ExerciseItemsMenuViewComponent : ViewComponent
+    {
+        private IExerciseService _exerciseService;
+        public ExerciseItemsMenuViewComponent(IExerciseService service)
+        {
+            _exerciseService = service;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var exercises = await _exerciseService.GetAllAsync();
+            var typeLists = exercises.GroupBy(exercise => exercise.ExerciseType);
+            
+            return View(typeLists);
+        }
+    }
+}
