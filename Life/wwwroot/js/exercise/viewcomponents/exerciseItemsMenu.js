42564edd9c08b17c27a0b66d3378f685a511f801
ArjenAch <arjenachterveld@hotmail.com>
2019-11-12 14:38:40 +0100

    Adds searchbar to exercise items ment & adds proper environmentscript usage

---
+++ Life/wwwroot/js/exercise/viewcomponents/exerciseItemsMenu.js
@@ -0,0 +1,19 @@
+﻿function searchMenuItems() {
+    // Declare variables
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById('searchInput');
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("menuList");
+    li = ul.getElementsByTagName('li');
+
+    // Loop through all list items, and hide those who don't match the search query
+    for (i = 0; i < li.length; i++) {
+        a = li[i];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
